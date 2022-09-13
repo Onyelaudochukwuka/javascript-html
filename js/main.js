fd3a59690be4bc7fe836b78f6998083a7e75d1f6
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 08:54:24 +0100

    adding packages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const getElement = (attribute) =>  document.querySelector(`[data-details = ${attribute}]`);
+require("dotenv").config();
+const getElement = (attribute) => document.querySelector(`[data-details = ${attribute}]`);
 const Github = {
     username: "Onyelaudochukwuka",
+    branch: "main",
     async getWorkflows() {
         let response = await fetch(
             `https://api.github.com/users/${this.username}?recursive=true`,
@@ -9,16 +11,25 @@ const Github = {
                 headers: {
                     "Content-Type": "application/json",
                     Accept: "application/vnd.github.v3+json",
+                    Authorization: "Bearer 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" 
                 
                 }
             }
         );
         return await response.json();
         
+    },
+    async getReadMe() {
+        let response = await fetch(
+            `https://raw.githubusercontent.com/${this.username}/${this.username}/${this.branch}/README.md`)
+
+            ;
+        return response.text();
     }
 }
 window.onload = async () => {
     const data = await Github.getWorkflows();
+    const readme = await Github.getReadMe();
     if (!!data) {
         setTimeout(() => {
             getElement("loader").style.transform = "translateX(-100%)";
@@ -26,4 +37,5 @@ window.onload = async () => {
     }
     getElement("heading").innerHTML = data.name;
     getElement("subheading").innerHTML = data.bio;
+    getElement("avatar").src = data.avatar_url;
 }
